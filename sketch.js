457d18fe1f54962824bc81c7777905785214bea5
Lil Ali <78896173+LilAliTech@users.noreply.github.com>
2021-02-23 15:47:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+var movingRect,fixedRect;
+
+
+function setup() {
+  createCanvas(1200,800);
+  movingRect = createSprite(400, 200, 50, 50);
+  movingRect.shapeColor = "blue";
+  fixedRect = createSprite(700, 200, 60, 50);
+  fixedRect.shapeColor = "blue";
+}
+
+function draw() {
+  background(0); 
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+
+  console.log(); // movingRect.x/2 + fixedRect.x/2
+
+  if(movingRect.x - fixedRect.x < movingRect.width/2 + fixedRect.width/2 && 
+    fixedRect.x - movingRect.x < movingRect.width/2 + fixedRect.width/2 &&
+    movingRect.y - fixedRect.y < movingRect.height/2 + fixedRect.height/2 &&
+    fixedRect.y - movingRect.y < movingRect.height/2 + fixedRect.height/2){
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }
+  else {
+    movingRect.shapeColor = "blue";
+    fixedRect.shapeColor = "blue";
+  }
+  drawSprites();
+}
